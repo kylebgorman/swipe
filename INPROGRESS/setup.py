88b7666e1685bc7bf501997ed3f389568c0add1b
Kyle Gorman <kylebgorman@gmail.com>
2011-09-12 22:23:44 -0400

    refresh

--- INPROGRESS/setup.py
+++
@@ -1,3 +0,0 @@
-from distutils.core import setup, Extension
-
-setup(name=swipe", version="1.0", ext_modules = [Extension("swipe", ["swipemodeul.c"]))
--- INPROGRESS/swipemodule.h
+++
@@ -1,3 +0,0 @@
-// Python function
-
-static PyObject* swipe_Swipe(PyObject *self, PyObject* args, PyObject* kwds);
